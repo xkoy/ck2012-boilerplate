cd9bc2d5c879a8747e219acd7f72368550f203e1
Andreas Isaak <andy.jared@googlemail.com>
2012-05-15 13:29:14 +0200

    Update ck2012 extension

--- system/modules/ck2012-boilerplate/config/config.php
+++ system/modules/ck2012-boilerplate/config/config.php
@@ -31,29 +31,31 @@
  */
 
 // Database settings
-$GLOBALS['TL_CONFIG']['dbPconnect'] = true;
-$GLOBALS['TL_CONFIG']['dbCharset']  = 'UTF8';
+$GLOBALS['TL_CONFIG']['dbPconnect']                 = true;
+$GLOBALS['TL_CONFIG']['dbCharset']                  = 'UTF8';
 
 // Localisation settings
-$GLOBALS['TL_CONFIG']['dateFormat']  = 'd.m.Y';
-$GLOBALS['TL_CONFIG']['datimFormat'] = 'd.m.Y H:i';
-$GLOBALS['TL_CONFIG']['timeZone']    = 'Europe/Berlin';
+$GLOBALS['TL_CONFIG']['dateFormat']                 = 'd.m.Y';
+$GLOBALS['TL_CONFIG']['datimFormat']                = 'd.m.Y H:i';
+$GLOBALS['TL_CONFIG']['timeZone']                   = 'Europe/Berlin';
 
 // URL settings
-$GLOBALS['TL_CONFIG']['rewriteURL']    = true;
-$GLOBALS['TL_CONFIG']['folderAlias']   = true;
-$GLOBALS['TL_CONFIG']['languageAlias'] = 'none';
+$GLOBALS['TL_CONFIG']['rewriteURL']                 = true;
+$GLOBALS['TL_CONFIG']['folderAlias']                = true;
+$GLOBALS['TL_CONFIG']['languageAlias']              = 'none';
+$GLOBALS['TL_CONFIG']['templateFiles']              = 'tpl,html5,xhtml,mobile';
 
 // Upload settings
-$GLOBALS['TL_CONFIG']['maxFileSize'] = 204800000;
-$GLOBALS['TL_CONFIG']['imageWidth']  = 3000;
-$GLOBALS['TL_CONFIG']['imageHeight'] = 3000;
+$GLOBALS['TL_CONFIG']['maxFileSize']                = 204800000;
+$GLOBALS['TL_CONFIG']['imageWidth']                 = 3000;
+$GLOBALS['TL_CONFIG']['imageHeight']                = 3000;
 
 // Image processing settings
-$GLOBALS['TL_CONFIG']['gdMaxImgWidth']  = PHP_INT_MAX;
-$GLOBALS['TL_CONFIG']['gdMaxImgHeight'] = PHP_INT_MAX;
+$GLOBALS['TL_CONFIG']['gdMaxImgWidth']              = PHP_INT_MAX;
+$GLOBALS['TL_CONFIG']['gdMaxImgHeight']             = PHP_INT_MAX;
 
 // Theme+ settings
-$GLOBALS['TL_CONFIG']['default_compression'] = 'gzip';
-$GLOBALS['TL_CONFIG']['default_css_minimizer'] = 'cssmin';
-$GLOBALS['TL_CONFIG']['default_js_minimizer'] = 'jsmin';
+$GLOBALS['TL_CONFIG']['default_compression']        = 'gzip';
+$GLOBALS['TL_CONFIG']['default_css_minimizer']      = 'cssmin';
+$GLOBALS['TL_CONFIG']['default_js_minimizer']       = 'jsmin';
+$GLOBALS['TL_CONFIG']['theme_plus_lesscss_mode']    = '-';
